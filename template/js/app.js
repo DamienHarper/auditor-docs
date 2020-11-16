e2dc75f626509c0a2d1702047275bc8d71c0b1dd
Damien Harper <damien.harper@gmail.com>
2020-11-16 01:01:06 +0100

    Couscous and custom template setup

---
+++ template/js/app.js
@@ -0,0 +1,51 @@
+$.when($.ready).then(function() {
+    // Close sidebar (mobile)
+    $(document).on('click', '#sidebar-close', function () {
+        $('#sidebar').addClass('hidden')
+        $('#sidebar-close').addClass('hidden')
+        $('#sidebar-open').removeClass('hidden')
+        $('#content-wrapper').removeClass('overflow-hidden max-h-screen fixed')
+    })
+
+    // Show sidebar (mobile)
+    $(document).on('click', '#sidebar-open', function () {
+        $('#sidebar-open').addClass('hidden')
+        $('#sidebar').removeClass('hidden')
+        $('#sidebar-close').removeClass('hidden')
+        $('#content-wrapper').addClass('overflow-hidden max-h-screen fixed')
+    })
+
+    // TOC
+    $(function() {
+        let elts = $('.markdown h2, .markdown h3, .markdown h4, .markdown h5');
+        if (elts.length > 0) {
+            $('#in-page-menu').removeClass('hidden');
+        }
+
+        elts.each(function () {
+            // prepend # to each section
+            var url = document.URL.replace(/#.*$/, "") + '#' + $(this).attr('id');
+            $(this).prepend(' <a class="title-anchor" href="' + url + '">#</a>');
+
+            // populate in-page-menu
+            var caption = $(this).text().substr(2);
+            var classes = 'mb-4 lg:mb-2';
+
+            switch ($(this)[0].nodeName) {
+                case 'H2':
+                    break;
+                case 'H3':
+                    classes += ' ml-4 lg:ml-2';
+                    break;
+                case 'H4':
+                    classes += ' ml-6 lg:ml-4';
+                    break;
+                case 'H5':
+                    classes += ' ml-8 lg:ml-6';
+                    break;
+            }
+
+            $('#in-page-menu ul').append('<li class="'+classes+'"><a class="block transition-fast hover:translate-r-2px hover:text-gray-900 font-medium text-gray-600" href="#' + $(this).attr('id') + '">' + caption + '</a></li>');
+        });
+    });
+});
